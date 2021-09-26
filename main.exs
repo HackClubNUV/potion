f449dd4ff6d75518b96fd7105cc2278bb487e4ab
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2021-09-26 15:53:31 +0400

    basic task

--- main.exs
+++ main.exs
@@ -1,1 +1,12 @@
-IO.puts("Hello world from Elixir")
+defmodule Testcode do
+    def main do
+        marks = IO.gets("Total Marks of 5 subjects(out of 500)-") |> String.trim
+        # Taking the user input and spliting it
+        percentage = fn (x, y) -> (x*(String.to_integer(marks)))/y end
+        # defining func percentage, where we convert string number to int
+        IO.puts "Percentage is: #{percentage.(100, 500)}"
+        # outputing the percentage and setting param x and y
+    end
+end
+Testcode.main() # running the module
+
